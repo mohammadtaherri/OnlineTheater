bff6da0c1d799fff0812c926d5c975f9f0ad9fc9
Mohammad Taheri <taherimohammad08@gmail.com>
2024-02-07 16:50:17 +0330

    add UnitOfWorkController

---
+++ OnlineTheater.Api/Controllers/UnitofWorkController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineTheater.Logic.Utils;
+
+namespace OnlineTheater.Api.Controllers;
+
+public class UnitofWorkController : ControllerBase
+{
+	private readonly UnitOfWork _unitOfWork;
+
+	public UnitofWorkController(UnitOfWork unitOfWork)
+	{
+		_unitOfWork = unitOfWork;
+	}
+
+	protected new IActionResult Ok()
+	{
+		_unitOfWork.SaveChanges();
+		return base.Ok(); ;
+	}
+
+	protected new IActionResult Ok<T>(T result)
+	{
+		_unitOfWork.SaveChanges();
+		return base.Ok(result); ;
+	}
+}
